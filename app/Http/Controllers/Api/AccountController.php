51bc18c6b9292583b6d549e37c1819710f8b3601
binhcute <75240946+binhcute@users.noreply.github.com>
2021-08-13 21:52:59 +0700

    api login

--- app/Http/Controllers/Api/AccountController.php
+++ app/Http/Controllers/Api/AccountController.php
@@ -6,6 +6,8 @@ use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Account as AccountResource;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class AccountController extends Controller
 {
@@ -68,5 +70,40 @@ class AccountController extends Controller
     {
         $user->delete();
         return response()->noContent();
+    }
+    public function register(Request $request)
+    {
+        // dd($request->all());
+        $user = new User();
+        $user->firstName = $request->firstName;
+        $user->lastName = $request->lastName;
+        $user->username = $request->username;
+        $user->avatar = $request->avatar;
+        $user->gender = $request->gender;
+        $user->phone = $request->phone;
+        $user->address = $request->address;
+        $user->email = $request->email;
+        $user->level = $request->level;
+        $user->password = Hash::make($request->password);
+        $user->status = $request->status;
+        $user->save();
+
+        return new AccountResource($user);
+    }
+
+    public function login(Request $request){
+        if(Auth::attempt([
+            'email' => $request->email,
+            'password' => $request->password,
+        ])){
+            $user = Auth::user();
+            // dd($user);
+
+            $accessToken = Auth::user()->createToken('authToken')->accessToken;
+// dd($accessToken);
+            // dd($user);
+            return response()->json(['user' => Auth::user(),'access_token'=>$accessToken]);
+        }
+        return response()->json(['error' => 'Username or password incorrect, please try again!'],401);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+Route::post('/login','Api\AccountController@login');
+Route::post('/register','Api\AccountController@register');
+
 Route::apiResource('product', 'Api\ProductController');
 Route::apiResource('category', 'Api\CategoryController');
 Route::apiResource('portfolio', 'Api\PortfolioController');
